e8ffd2bc32efdfc0d33afe497fd3dc06d547223a
Tomás Rojas <tmsrjs@gmail.com>
2024-10-24 17:28:02 -0300

    Extract logic for onChain filler

--- typescript/solver/fillers/onChain.ts
+++ typescript/solver/fillers/onChain.ts
@@ -1,48 +1,30 @@
 import { Contract } from "@ethersproject/contracts";
 import { Wallet } from "@ethersproject/wallet";
 import { ensure0x } from "@hyperlane-xyz/utils";
+import { chainMetadata } from '@hyperlane-xyz/registry';
+import { MultiProvider } from '@hyperlane-xyz/sdk';
 
 import DESTINATION_SETTLER_ABI from "../abi/destinationSettler";
 
-import type { OpenEventArgs } from "../types";
+import type { FillInstruction, OpenEventArgs, Output } from "../types";
 
 const create = () => {
-  const { wallet } = setup();
+  const { multiProvider } = setup();
 
   return async function onChain({ orderId, resolvedOrder }: OpenEventArgs) {
     const { maxSpent: outputs, minReceived: inputs, fillInstructions } = resolvedOrder;
 
-    // It's still not clear if there MUST be an input for every output, so maybe it doesn't make any
-    // sense to think about it this way, but we somehow need decide whether exchanging `inputs` for
-    // `outputs` is a good deal for us.
-    await Promise.all(inputs.map(async (input, index): Promise<void> => {
-      const output = outputs[index];
-      console.log(input.token, input.amount, output.token, output.amount);
-    }));
+    if (!(await isProfitable(inputs, outputs, multiProvider))) {
+      console.log("Not profitable");
+      return;
+    }
 
-    // We're assuming the filler will pay out of their own stock, but in reality they may have to
-    // produce the funds before executing each leg.
-    await Promise.all(outputs.map(async (output): Promise<void> => {
-      // const multiProvider = new MultiProvider(chainMetadata);
-      // const filler = multiProvider.setSharedSigner(wallet);
+    if (!(await hasEnoughFunds(outputs, multiProvider))) {
+      console.log("Not enough funds");
+      return;
+    }
 
-      // Check filler has at least output.amount of output.token available for executing this leg.
-      console.log(wallet.address, output.token, output.amount);
-    }));
-
-    await Promise.all(outputs.map(async (output, index): Promise<void> => {
-      // const multiProvider = new MultiProvider(chainMetadata);
-      // const filler = multiProvider.setSharedSigner(wallet);
-
-      const destinationSettler = fillInstructions[index].destinationSettler;
-      const destination = new Contract(destinationSettler, DESTINATION_SETTLER_ABI, wallet);
-
-      const originData = fillInstructions[index].originData;
-      // Depending on the implementation we may call `destination.fill` directly or call some other
-      // contract that will produce the funds needed to execute this leg and then in turn call
-      // `destination.fill`
-      await destination.fill(orderId, originData, "");
-    }));
+    await fill(orderId, outputs, fillInstructions, multiProvider);
   }
 }
 
@@ -50,14 +32,53 @@ function setup() {
   const privateKey = process.env.PRIVATE_KEY;
   const mnemonic = process.env.MNEMONIC;
 
-  if (!privateKey || !mnemonic) {
+  if (!privateKey && !mnemonic) {
     throw new Error("Either a private key or mnemonic must be provided");
   }
 
-  const wallet = privateKey ? new Wallet(ensure0x(privateKey)) : Wallet.fromMnemonic(mnemonic);
+  const multiProvider = new MultiProvider(chainMetadata);
+  const wallet = privateKey ? new Wallet(ensure0x(privateKey)) : Wallet.fromMnemonic(mnemonic!);
+  multiProvider.setSharedSigner(wallet);
 
-  return { wallet };
+  return { multiProvider };
+}
+
+async function isProfitable(inputs: Array<Output>, outputs: Array<Output>, multiProvider: MultiProvider): Promise<boolean> {
+  // It's still not clear if there MUST be an input for every output, so maybe it doesn't make any
+  // sense to think about it this way, but we somehow need to decide whether exchanging `inputs` for
+  // `outputs` is a good deal for us.
+  await Promise.all(inputs.map(async (input, index): Promise<void> => {
+    const output = outputs[index];
+    const provider = multiProvider.getProvider(output.chainId);
+    console.log(await provider.getBlockNumber(), input.token, input.amount, output.token, output.amount);
+  }));
+  return true;
+}
+
+async function hasEnoughFunds(outputs: Array<Output>, multiProvider: MultiProvider): Promise<boolean> {
+  // We're assuming the filler will pay out of their own stock, but in reality they may have to
+  // produce the funds before executing each leg.
+  await Promise.all(outputs.map(async (output): Promise<void> => {
+    const filler = multiProvider.getSigner(output.chainId);
+    // Check filler has at least output.amount of output.token available for executing this leg.
+    console.log(await filler.getAddress(), output.token, output.amount);
+  }));
+  return true;
+}
+
+async function fill(orderId: string, outputs: Array<Output>, fillInstructions: Array<FillInstruction>, multiProvider: MultiProvider): Promise<void> {
+  await Promise.all(outputs.map(async (output, index): Promise<void> => {
+    const filler = multiProvider.getSigner(output.chainId);
+
+    const destinationSettler = fillInstructions[index].destinationSettler;
+    const destination = new Contract(destinationSettler, DESTINATION_SETTLER_ABI, filler);
+
+    const originData = fillInstructions[index].originData;
+    // Depending on the implementation we may call `destination.fill` directly or call some other
+    // contract that will produce the funds needed to execute this leg and then in turn call
+    // `destination.fill`
+    await destination.fill(orderId, originData, "");
+  }));
 }
 
 export const onChain = { create };
-
