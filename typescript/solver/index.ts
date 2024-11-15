e571a68f3a27e2361266b52622f93dcdb9d0b87a
fernandomg <fernando.greco@gmail.com>
2024-11-15 12:00:27 +0100

    create SolverWallet to handle nonces race conditions
    Co-Authored-By: Tomás Rojas <tmsrjs@gmail.com>

--- typescript/solver/index.ts
+++ typescript/solver/index.ts
@@ -6,20 +6,24 @@ import { LogFormat, Logger, LogLevel } from "./logger.js";
 import * as solvers from "./solvers/index.js";
 
 const log = new Logger(LogFormat.Pretty, LogLevel.Info);
+import { getMultiProvider } from "./solvers/utils.js";
 
 log.title("🙍 Intent Solver 📝");
 
-const main = () => {
+const main = async () => {
   log.subtitle("Starting...", "\n");
+
+  const multiProvider = await getMultiProvider();
 
   // TODO: implement a way to choose different listeners and fillers
   const ecoListener = solvers["eco"].listener.create();
-  const ecoFiller = solvers["eco"].filler.create();
+  const ecoFiller = solvers["eco"].filler.create(multiProvider);
 
   ecoListener(ecoFiller);
 
   const hyperlane7683Listener = solvers["hyperlane7683"].listener.create();
-  const hyperlane7683Filler = solvers["hyperlane7683"].filler.create();
+  const hyperlane7683Filler =
+    solvers["hyperlane7683"].filler.create(multiProvider);
 
   hyperlane7683Listener(hyperlane7683Filler);
 };
--- typescript/solver/solvers/utils.ts
+++ typescript/solver/solvers/utils.ts
@@ -1,7 +1,58 @@
 import fs from "node:fs";
+
+import { Wallet } from "@ethersproject/wallet";
+import { chainMetadata } from "@hyperlane-xyz/registry";
+import { MultiProvider } from "@hyperlane-xyz/sdk";
+import { ensure0x, Result } from "@hyperlane-xyz/utils";
 import { parse } from "yaml";
+
+import { MNEMONIC, PRIVATE_KEY } from "../config.js";
+import { SolverWallet } from "../SolverWallet.js";
 
 export function getMetadata<TMetadata>(dirname: string): TMetadata {
   const data = fs.readFileSync(`${dirname}/metadata.yaml`, "utf8");
   return parse(data);
+}
+
+export async function getMultiProvider() {
+  if (!PRIVATE_KEY && !MNEMONIC) {
+    throw new Error("Either a private key or mnemonic must be provided");
+  }
+
+  const multiProvider = new MultiProvider(chainMetadata);
+  const wallet = (
+    PRIVATE_KEY
+      ? new SolverWallet(ensure0x(PRIVATE_KEY))
+      : SolverWallet.fromMnemonic(MNEMONIC!)
+  ) as SolverWallet;
+
+  const chainIds = multiProvider.getKnownChainIds().filter(Number).map(Number);
+  const nonces = await retrieveChainsNonce(chainIds, wallet, multiProvider);
+
+  wallet.setupNonceCache(chainIds, nonces);
+
+  multiProvider.setSharedSigner(wallet);
+
+  return multiProvider;
+}
+
+async function retrieveChainsNonce(
+  chainIds: Array<number>,
+  wallet: Wallet,
+  multiProvider: MultiProvider,
+): Promise<Array<Result<number>>> {
+  const nonces = await Promise.allSettled(
+    chainIds.map(async (chainId) => {
+      const provider = multiProvider.getProvider(chainId);
+      return provider.getTransactionCount(wallet.address);
+    }),
+  );
+
+  return nonces.map((result) => {
+    if (result.status === "fulfilled") {
+      return { success: true, data: result.value };
+    } else {
+      return { success: false, error: result.reason };
+    }
+  });
 }
