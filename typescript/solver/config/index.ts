83b1736e06661eb8b82e865f9374c0a7e063e033
Lisandro Corbalan <lmcorbalan@gmail.com>
2024-12-06 15:02:55 -0300

    review changes

--- typescript/solver/config/index.ts
+++ typescript/solver/config/index.ts
@@ -8,7 +8,7 @@ const LOG_LEVEL = process.env.LOG_LEVEL;
 const MNEMONIC = process.env.MNEMONIC;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-export { LOG_FORMAT, LOG_LEVEL, MNEMONIC, PRIVATE_KEY, allowBlockListsGlobal };
+export { LOG_FORMAT, LOG_LEVEL, MNEMONIC, PRIVATE_KEY };
 
 type GenericAllowBlockListItem = {
   [key: string]: string[] | "*";
@@ -23,13 +23,9 @@ type Item = {
   [Key in keyof GenericAllowBlockListItem]: string;
 };
 
-const matches = (item: GenericAllowBlockListItem, data: Item): boolean => {
-  const matches = Object.entries(item).map(([key, value]) => {
-    return value === "*" || value.includes(data[key])
-  })
-
-  return matches.every((el: boolean) => el)
-};
+const matches = (item: GenericAllowBlockListItem, data: Item): boolean => (
+  Object.entries(item).every(([key, value]) => (value === "*" || value.includes(data[key])))
+)
 
 export function isAllowedIntent(
   allowBlockLists: GenericAllowBlockLists,
