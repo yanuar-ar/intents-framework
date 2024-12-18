84c9cc64d516686b60a513f3ba7ce7e6e5af19d9
fernandomg <fernando.greco@gmail.com>
2024-12-18 21:55:56 +0100

    support native

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,6 +1,8 @@
 import { type WarpCoreConfig } from '@hyperlane-xyz/sdk';
+import { zeroAddress } from 'viem';
 
-const ROUTER = '0x376dc8e71a223af488d885ce04a7021f32c2d1e0';
+// const ROUTER = '0x376dc8e71a223af488d885ce04a7021f32c2d1e0';
+const ROUTER = '0xe0c8f83bA0686FDF1a76AF0cC202181AEaA25a03';
 const ITT = '0x5f94BC7Fb4A2779fef010F96b496cD36A909E818';
 
 // A list of Warp Route token configs
@@ -38,6 +40,38 @@ export const warpRouteConfigs: WarpCoreConfig = {
       name: 'ITT',
       standard: 'Intent',
       symbol: 'ITT',
+      protocol: 'ethereum',
+    },
+    {
+      addressOrDenom: zeroAddress,
+      chainName: 'optimismsepolia',
+      collateralAddressOrDenom: ROUTER,
+      connections: [
+        {
+          token: 'ethereum|basesepolia|' + zeroAddress,
+        },
+      ],
+      decimals: 18,
+      logoURI: '/deployments/warp_routes/ETH/logo.svg',
+      name: 'ETH',
+      standard: 'IntentNative',
+      symbol: 'ETH',
+      protocol: 'ethereum',
+    },
+    {
+      addressOrDenom: zeroAddress,
+      chainName: 'basesepolia',
+      collateralAddressOrDenom: ROUTER,
+      connections: [
+        {
+          token: 'ethereum|optimismsepolia|' + zeroAddress,
+        },
+      ],
+      decimals: 18,
+      logoURI: '/deployments/warp_routes/ETH/logo.svg',
+      name: 'ETH',
+      standard: 'IntentNative',
+      symbol: 'ETH',
       protocol: 'ethereum',
     },
   ],
