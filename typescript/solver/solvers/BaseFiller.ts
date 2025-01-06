b5a1bd0c401c2b66761561b9a9997d7f4f88c71a
fernandomg <fernando.greco@gmail.com>
2025-01-06 17:15:01 +0100

    feat: define BaseFiller class

---
+++ typescript/solver/solvers/BaseFiller.ts
@@ -0,0 +1,73 @@
+import type { MultiProvider } from "@hyperlane-xyz/sdk";
+import type { Result } from "@hyperlane-xyz/utils";
+import type { Logger } from "../logger.js";
+
+type Metadata = {
+  protocolName: string;
+};
+
+type ParsedArgs = {
+  orderId: string;
+};
+
+export abstract class BaseFiller<
+  TMetadata extends Metadata,
+  TParsedArgs extends ParsedArgs,
+  TIntentData extends unknown,
+> {
+  protected constructor(
+    readonly multiProvider: MultiProvider,
+    readonly metadata: TMetadata,
+    readonly log: Logger,
+  ) {}
+
+  create() {
+    return async (parsedArgs: TParsedArgs, originChainName: string) => {
+      const origin = await this.retrieveOriginInfo(parsedArgs, originChainName);
+      const target = await this.retrieveTargetInfo(parsedArgs);
+
+      this.log.info({
+        msg: "Intent Indexed",
+        intent: `${this.metadata.protocolName}-${parsedArgs.orderId}`,
+        origin: origin.join(", "),
+        target: target.join(", "),
+      });
+
+      const intent = await this.prepareIntent(parsedArgs);
+
+      if (!intent.success) {
+        this.log.error(`Failed evaluating filling Intent: ${intent.error}`);
+        return;
+      }
+
+      const { data } = intent;
+
+      await this.fill(parsedArgs, data, originChainName);
+
+      await this.settleOrder(parsedArgs, data);
+    };
+  }
+
+  protected abstract retrieveOriginInfo(
+    parsedArgs: TParsedArgs,
+    originChainName: string,
+  ): Promise<Array<string>>;
+
+  protected abstract retrieveTargetInfo(
+    parsedArgs: TParsedArgs,
+  ): Promise<Array<string>>;
+
+  protected abstract prepareIntent(
+    parsedArgs: TParsedArgs,
+  ): Promise<Result<TIntentData>>;
+
+  protected abstract fill(
+    parsedArgs: TParsedArgs,
+    data: TIntentData,
+    originChainName: string,
+  ): Promise<void>;
+
+  protected async settleOrder(parsedArgs: TParsedArgs, data: TIntentData) {
+    return;
+  }
+}
