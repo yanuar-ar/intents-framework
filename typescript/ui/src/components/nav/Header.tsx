c2c53b9f3efdde8d3799f14cf697f5ad49422f32
Gabito Esmiapodo <gabriel@bootnode.dev>
2025-02-21 15:08:13 -0300

    fix: header items vertical alignment

--- typescript/ui/src/components/nav/Header.tsx
+++ typescript/ui/src/components/nav/Header.tsx
@@ -8,7 +8,7 @@ import Title from '../../images/logos/app-title.svg';
 export function Header() {
   return (
     <header className="w-full px-2 pb-2 pt-3 sm:px-6 lg:px-12">
-      <div className="flex items-start justify-between">
+      <div className="flex items-center justify-between">
         <Link href="/" className="flex items-center py-2">
           <Image src={Logo} height={40} alt="" />
           <Image src={Name} height={30} alt="" className="ml-2 mt-0.5 hidden sm:block" />
